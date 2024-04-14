04d4f7f60fc446d36149ed5ca1f50e2eb5980bac
23tae <taehoonkim.dev@gmail.com>
2024-04-14 22:54:18 +0900

    Feat: POST API 구현

--- src/main/kotlin/com/example/demo/datasource/mock/MockBankDataSource.kt
+++ src/main/kotlin/com/example/demo/datasource/mock/MockBankDataSource.kt
@@ -3,12 +3,13 @@ package com.example.demo.datasource.mock
 import com.example.demo.datasource.BankDataSource
 import com.example.demo.model.Bank
 import org.springframework.stereotype.Repository
+import java.lang.IllegalArgumentException
 import java.util.NoSuchElementException
 
 @Repository
 class MockBankDataSource : BankDataSource {
 
-    val banks = listOf(
+    val banks = mutableListOf(
         Bank("1234", 3.14, 17),
         Bank("1010", 12.0, 10),
         Bank("5678", 23.0, 100)
@@ -18,4 +19,12 @@ class MockBankDataSource : BankDataSource {
 
     override fun retrieveBank(accountNumber: String): Bank = banks.firstOrNull() { it.accountNumber == accountNumber }
         ?: throw NoSuchElementException("could not find a bank with account number $accountNumber")
+
+    override fun createbank(bank: Bank): Bank {
+        if (banks.any { it.accountNumber == bank.accountNumber }) {
+            throw IllegalArgumentException("Bank with account number ${bank.accountNumber} already exists")
+        }
+        banks.add(bank)
+        return bank
+    }
 }
